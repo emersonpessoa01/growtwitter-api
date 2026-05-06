2bbd176f6b8f703c425e8035ade01206d4ddc703
𝐄𝐦𝐞𝐫𝐬𝐨𝐧 𝐏𝐞𝐬𝐬𝐨𝐚 <60630092+emersonpessoa01@users.noreply.github.com>
2026-05-06 13:40:14 -0300

    fix: Corrigido a tipagem do setup do Swagger e ajustados links de CDN. :wrench:

--- src/server.ts
+++ src/server.ts
@@ -68,6 +68,8 @@ serverInstance.app.use(
   swaggerUi.setup(swaggerSpec, {
     customCssUrl:
       "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+    customJs:
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js",
     customSiteTitle: "Growtwitter API Docs",
   }),
 );
